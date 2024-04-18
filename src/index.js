8c2f260eccad290deeab4c806cae460fc3c917d5
Ale <alejararara@gmail.com>
2024-04-18 19:41:33 -0400

    Distribuidos

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const axios= require('axios');
+const responseTime = require('response-time');
+const redis= require('redis');
+
+const client = redis.createClient({
+    host: "localhost",
+    port:6379,
+
+});
+
+const app = express();
+
+app.use(responseTime());
+
+
+app.get('/pokemon', async (req,res) =>{
+
+
+    const response = await axios.get("https://pokeapi.co/api/v2/pokemon/?limit=10277");
+    client.set('pokemones', JSON.stringify(response.data), (err, reply) => {
+        
+        if (err) console.log(err);
+
+        console.log(reply);
+
+        res.json(response.data);
+
+    });    
+});
+
+app.listen(3000);
+console.log('server in port 3000');
